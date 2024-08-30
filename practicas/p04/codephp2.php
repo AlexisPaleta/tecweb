d413636181432176217979b5388260c5cca0caea
Alexis Paleta <157434896+AlexisPaleta@users.noreply.github.com>
2024-08-30 11:36:15 -0600

    Quitar texto innecesario

--- practicas/p04/codephp2.php
+++ practicas/p04/codephp2.php
@@ -18,7 +18,7 @@
         echo "<h2> Titulo escrito por PHP </h2>";
         $variable2="MySQL";
     ?>
-    <p>Vas a descubrir <?= $variable1 ?> ←(5)</p>
+    <p>Vas a descubrir <?= $variable1 ?></p>
     <?php
         echo "<h2> Buenos días de $variable1 </h2>";
     ?>
@@ -27,5 +27,5 @@
         echo $variable2;
     ?>
     </p>
-    <?= "<div><big> Buenos días de $variable2 </big></div>" ?> ← (8)
+    <?= "<div><big> Buenos días de $variable2 </big></div>" ?>
 </body>

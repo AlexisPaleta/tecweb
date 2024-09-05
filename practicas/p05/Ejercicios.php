92aa853b9cca3500a821fbb49cd319a11462542e
Alexis Paleta <157434896+AlexisPaleta@users.noreply.github.com>
2024-09-04 21:01:46 -0600

    Ejercicio 2

--- practicas/p05/Ejercicios.php
+++ practicas/p05/Ejercicios.php
@@ -8,6 +8,8 @@
 <body>
 <?php
 //EJERCICIO 1:
+print_r("Ejercicio 1:");
+echo "<br>";
 //Determina cuál de las siguientes variables son válidas y explica por qué:
 $_myvar = 1; //su nombre empieza por _ lo que es permitido
 print_r("\$_myvar = 1; //su nombre empieza por _ lo que es permitido");
@@ -31,6 +33,30 @@ echo "<br>";
 print_r("\$house*5 = 1 // da Error por el *");
 echo "<br>";
 
+print_r("Ejercicio 2:");
+echo "<br>";
+$a = "ManejadorSQL";
+$b = 'MySQL';
+$c = &$a;
+
+print_r("Contenido de a = $a");
+echo "<br>";
+print_r("Contenido de a = $b");
+echo "<br>";
+print_r("Contenido de a = $c");
+echo "<br>";
+
+print_r("MODIFICANDO...");
+echo "<br>";
+$a = "PHP SERVER";
+$b = &$a;
+print_r("Contenido de a = $a");
+echo "<br>";
+print_r("Contenido de a = $b");
+echo "<br>";
+print_r("Contenido de a = $c");
+echo "<br>";
+print_r("Lo que ocurrió fue que al inicio a poseia una cadena, b otra aparte, pero a c se le asignó la dirección de memoria de a, por lo que entonces ahora c contiene lo que este guardado en la variable a, eso se muestra en la primera impresión, después se ve cómo al asignar en b la dirección de memoria de 'a' también, ahora si se modifica 'a' cambiarán los 3 ");
 
 ?>
 </body>

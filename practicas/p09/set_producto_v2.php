532f8f16ad7856621e4622349d4d28b1f19b1b4c
Alexis Paleta <157434896+AlexisPaleta@users.noreply.github.com>
2024-09-23 22:49:27 -0600

    Agregar comentario de la consulta anterior

--- practicas/p09/set_producto_v2.php
+++ practicas/p09/set_producto_v2.php
@@ -26,7 +26,7 @@ if ( $result = $link->query("SELECT * FROM productos WHERE nombre='{$nombre}' an
 			if($result->num_rows > 0){
                 echo "<h1>ERROR: YA HAY DATOS REGISTRADOS CON ESE NOMBRE-MARCA-MODELO</h1>";
             }else{
-
+                //Consulta anterior: $sql = "INSERT INTO productos VALUES (null,'{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', '{$eliminado}')";
                 $sql = "INSERT INTO productos(nombre,marca,modelo,precio,detalles,unidades,imagen,eliminado) VALUES ('{$nombre}', '{$marca}', '{$modelo}', {$precio}, '{$detalles}', {$unidades}, '{$imagen}', '{$eliminado}')";
                 if ( $link->query($sql) ) 
                 {
@@ -34,7 +34,7 @@ if ( $result = $link->query("SELECT * FROM productos WHERE nombre='{$nombre}' an
                 else
                 {
                     echo 'El Producto no pudo ser insertado =(';
-                }_
+                }
 
                 echo "<h1>Los Datos ingredados fueron:</h1>";
                 echo "<p>Nombre: $nombre</p>";

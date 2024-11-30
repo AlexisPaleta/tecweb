f74ce8fc118de964af5e37836f3a4d3685e36bbc
Alexis Paleta <157434896+AlexisPaleta@users.noreply.github.com>
2024-11-29 19:10:16 -0600

    slim método POST

--- practicas/p15/vendor/index.php
+++ practicas/p15/vendor/index.php
@@ -13,5 +13,13 @@
         return $response;
     });
 
+    $app->post('/pruebapost', function($request, $response, $args){
+        $reqPost = $request->getParsedBody();
+        $val1 = $reqPost['val1'];
+        $val2 = $reqPost['val2'];
+
+        $response->write('Valores: ' . $val1 . " " . $val2);
+        return $response;
+    });
     $app->run();
 ?>
